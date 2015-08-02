30ec217f51c5b85b28a885b276c41e660397689a
Ultima <ulrichabiguime@qq.com>
2015-08-02 22:38:59 +0800

    Commit Project

--- app/src/main/java/sz/ultima/imageu/activity/MainActivity.java
+++ app/src/main/java/sz/ultima/imageu/activity/MainActivity.java
@@ -1,9 +1,11 @@
-package sz.ultima.imageu;
+package sz.ultima.imageu.activity;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import sz.ultima.imageu.R;
 
 
 public class MainActivity extends ActionBarActivity {
