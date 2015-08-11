c395c5983038f36761f55c58ae7bbcc10be090c2
Ulrich Abiguime <ulrichabiguime@qq.com>
2015-08-11 17:58:21 +0800

    11Aout.

--- app/src/main/java/sz/ulrichabiguime/imageu/activity/fragment/ShidoActivityFragment.java
+++ app/src/main/java/sz/ulrichabiguime/imageu/activity/fragment/ShidoActivityFragment.java
@@ -1,12 +1,12 @@
-package sz.ultima.imageu.activity.fragment;
-
+package sz.ulrichabiguime.imageu.activity.fragment;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import sz.ultima.imageu.R;
+import sz.ulrichabiguime.imageu.R;
+
 
 /**
  * A placeholder fragment containing a simple view.
